4f540c0da633f41d0cbd7f6253eefcc6aaeb2e4c
Nilton Clasen <nduducbnu@gmail.com>
2024-09-17 21:12:58 -0300

    first commit

---
+++ src/main/java/br/com/fiap/park_tech/ParkTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.park_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkTechApplication.class, args);
+	}
+
+}
